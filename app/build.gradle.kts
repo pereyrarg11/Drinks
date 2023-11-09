e67697e96f62d6210e46b0706ea30277ca150a1d
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-11-09 14:01:23 -0600

    chore: increase app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,11 @@ android {
         applicationId = "com.pereyrarg11.drinks"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        // 1_0001_2_3
+        // Major Minor  Patch  Release candidate
+        // 1     0001   2      3
+        versionCode = 1_0000_0_1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
